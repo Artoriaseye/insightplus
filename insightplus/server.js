7b171b01fe5e8c4dba30cdf17af7c234f541559a
artoriaseye <artoriaseye@gmail.com>
2018-07-05 15:52:53 +0800

    2018.7.5

--- insightplus/server.js
+++ insightplus/server.js
@@ -1,8 +1,12 @@
 'use strict';
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    let filePath = path.join(__dirname, '/static/index.html');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    let readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
 }).listen(port);
